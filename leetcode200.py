59e754e6e444873d1b36293db1c8f557f5618810
Lucas Babel <33047456+Lmbabel@users.noreply.github.com>
2020-04-08 16:26:20 -0500

    Old saved solutions

---
+++ leetcode200.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 00:03:30 2020
+
+@author: Lucas
+"""
+
+grid = [[1, 2, 0, 0, 0],
+        [6, 7, 0, 0, 0],
+        [0, 0, 13, 0, 0],
+        [0, 0, 0, 19, 20]]
+def dfs(_grid, j, i):
+    if j < 0 or i < 0 or j >= len(grid) or i >= len(grid[0]) or grid[j][i] == 0:
+        return
+    grid[j][i] = '#'
+    dfs(grid, j+1, i)
+    dfs(grid, j-1, i)
+    dfs(grid, j, i+1)
+    dfs(grid, j, i-1)
+    
+
+if not grid:
+    print(0)
+
+count = 0
+for y in range(len(grid)):
+    for x in range(len(grid[0])):
+        if grid[y][x] != 0:
+            dfs(grid, y, x)
+            count += 1
+print(count)
+
+
+
+
+
+
+
+    
+    
---
+++ pythag_triple.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 12:43:21 2020
+
+@author: Lucas
+"""
+## Find pythagorean triples in an unsorted array
+
+arr = [3, 1, 4, 6, 5]
+
