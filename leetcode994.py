baca93f02df5e8c50a5612f07540ebf4ab04312b
Lucas Babel <33047456+Lmbabel@users.noreply.github.com>
2020-04-09 22:55:54 -0500

    outline

---
+++ leetcode994.py
@@ -0,0 +1,10 @@
+## Rotting oranges problem: https://leetcode.com/problems/rotting-oranges/ ##
+
+sampleInput = [[2,1,1],[0,1,1],[1,0,1]]
+
+## Attempt 1, O(n^2) ##
+
+def bfs(grid, x, y):
+        
+
+
